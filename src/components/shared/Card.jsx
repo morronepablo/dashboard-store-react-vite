527d1223e4e45714be0de823874ccf8856ddffe1
Pablo Morrone <pmorrone@it.nortia.com.ar>
2023-07-07 17:28:54 -0300

    subiendo proyecto final

---
+++ src/components/shared/Card.jsx
@@ -0,0 +1,20 @@
+
+
+const Card = (props) => {
+    const {img, description, price, inventory} = props;
+
+    return (
+      <div className="bg-[#1F1D2B] p-10 rounded-xl flex flex-col items-center gap-2 text-center text-gray-300">
+        <img
+          src={img}
+          alt={img}
+          className="w-40 h-40 object-cover -mt-20 shadow-2xl rounded-full"
+        />
+        <p className="text-xl">{description}</p>
+        <span className="text-gray-400">${price}</span>
+        <p className="text-gray-600">{inventory} Bowls available</p>
+      </div>
+    );
+}
+
+export default Card
